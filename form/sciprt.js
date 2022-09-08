99669246a988516fdb9152214b2a353d678568a4
Utkarsh Lubal <86215353+ubiiii@users.noreply.github.com>
2022-09-08 10:30:23 +0530

    Add files via upload

---
+++ form/sciprt.js
@@ -0,0 +1,24 @@
+let fileInput = document.getElementById("file-input");
+
+let imageContainer document.getElementById("images");
+let numOfFiles = document.getElementById("num-of-files");
+
+
+function preview(){
+	imageContainer.innerHTML = "";
+	numOfFiles.textContent = `${fileInput.files.length} 
+	Files Selected`;
+	
+	for(1 of fileInput.files){
+		let reader = new FileReader();
+		let figure= document.createElement("figure"); 
+		let figCap= document.createElement("figcaption"); 
+		figCap.innerText = 1.name;
+		figure.appendChild (figCap); reader.onload=()=>{
+			let img = document.createElement("img"); 
+			img.setAttribute("src", reader.result); 
+			figure.insertBefore(img, figCap);
+		}
+		imageContainer.appendChild(figure);
+		reader.readAsDataURL (i);
+	}
